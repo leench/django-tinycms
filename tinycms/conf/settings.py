678d3d6edb7a0e23e5b9da732582c10ef5a3b560
Lee Lin <leench@gmail.com>
2013-06-20 09:59:42 +0800

    tinycms's settings file can load from django settings file

--- tinycms/conf/settings.py
+++ tinycms/conf/settings.py
@@ -1,3 +1,5 @@
-PUBLISH_DEFAULT     = True
-DRAFT_DEFAULT       = False
-TEMPLATE_DEFAULT    = 'default'
+from django.conf import settings
+
+PUBLISH_DEFAULT     = getattr(settings, "PUBLISH_DEFAULT", True)
+DRAFT_DEFAULT       = getattr(settings, "DRAFT_DEFAULT", False)
+TEMPLATE_DEFAULT    = getattr(settings, "TEMPLATE_DEFAULT", "default")
